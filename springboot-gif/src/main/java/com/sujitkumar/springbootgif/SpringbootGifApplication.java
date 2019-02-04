47a73e68991ca43c260557de5ec5622d090ffb21
Sujit Kumar <11oct8krsujit@gmail.com>
2019-02-04 23:32:18 +0530

    Spring boot demo

---
+++ springboot-gif/src/main/java/com/sujitkumar/springbootgif/SpringbootGifApplication.java
@@ -0,0 +1,14 @@
+package com.sujitkumar.springbootgif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGifApplication.class, args);
+	}
+
+}
+
