d89db3f048329589bbdee6df5bf9c0e17bd0153f
Sujit Kumar <11oct8krsujit@gmail.com>
2019-02-04 22:11:53 +0530

    First commit

---
+++ src/main/java/com/sujitkumar/springbootgif/SpringbootGifApplication.java
@@ -0,0 +1,17 @@
+package com.sujitkumar.springbootgif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@SpringBootApplication
+public class SpringbootGifApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGifApplication.class, args);
+	}
+
+}
+
